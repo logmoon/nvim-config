e450ef7c60aaa55f94c00c46be5f2e158e38176c
Amen <53493383+logmoon@users.noreply.github.com>
2026-03-06 17:29:28 +0100

    life better

--- init.lua
+++ init.lua
@@ -246,7 +246,7 @@ vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+d<CR>')
 
 vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>")
 vim.keymap.set("n", "<leader>p",  ":PasteImage<CR>")
-vim.keymap.set("n", "<leader>td", toggle_diagnostics)
+vim.keymap.set("n", "<leader>dt", toggle_diagnostics)
 vim.keymap.set("n", "<leader>e",  ":Oil<CR>")
 vim.keymap.set("n", "<leader>o",  ":!" .. open_cmd .. " %<CR><CR>")
 
